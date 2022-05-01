ea6f4a1448cd1cff1354685ecc2efbde0fb63b0f
sjm150 <sjm150@snu.ac.kr>
2022-05-01 17:32:58 +0900

    Implemented to_pig_latin

--- Book_Exercises/Ch8/to_pig_latin/src/main.rs
+++ Book_Exercises/Ch8/to_pig_latin/src/main.rs
@@ -1,3 +1,41 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("------------------------------");
+    println!(" Convert strings to pig latin ");
+    println!("------------------------------");
+    println!("Input white space or just enter to terminate");
+
+    loop {
+        print!("String: ");
+        io::stdout().flush().ok().expect("flush");
+
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).expect("read_line");
+    
+        let mut chars = buf.trim().chars();
+        match chars.next() {
+            Some(c) => {
+                print!("     -> ");
+                if is_vowel(c) {
+                    println!("{}{}-hay", c, chars.as_str());
+                } else {
+                    println!("{}-{}ay", chars.as_str(), c);
+                }
+            }
+           None => {
+                println!("Terminate");
+                break;
+            }
+        }
+    }
+}
+
+fn is_vowel(c: char) -> bool {
+    if c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U' 
+    || c == 'Α' {
+        true
+    } else {
+        false
+    }
 }
